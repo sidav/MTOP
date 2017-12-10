325f6538d945023da7adb25f286aab9331c09723
sidav <sidav94@gmail.com>
2017-12-10 19:19:25 +0300

    I dunno lol

--- Runge-Kutt.py
+++ Runge-Kutt.py
@@ -5,10 +5,10 @@ x0 = 0.0 #задать
 y0 = 0.0 #задать
 h = 0.001 #шаг сетки Х
 
-def f_x_y(x, y):
+def crap(x, y):
     return 2.0 * x
 
-def nexty(h, xn, yn):
+def nexty(f_x_y, h, xn, yn):
     k1 = f_x_y(xn,yn)
     k2 = f_x_y(xn + h/2.0, yn + (h/2.0)*k1)
     k3 = f_x_y(xn + h/2, yn + (h/2)*k2)
@@ -25,9 +25,8 @@ def solveEquation():
     xVector[0] = x0
     yVector[0] = y0
     for i in range(len(xVector)-1):
-        yVector[i+1] = nexty(h, xVector[i], yVector[i])
+        yVector[i+1] = nexty(crap, h, xVector[i], yVector[i])
         xVector[i+1] = xVector[i]+h
-    print("zomg")
     plt.plot(xVector, yVector)
     plt.show()
     # for i in range(len(xVector)):
@@ -35,6 +34,7 @@ def solveEquation():
 
 plt.xlim(0, 10)
 plt.ylim(0, 10)
+plt.grid(linewidth=1)
 solveEquation()
 
 #

7f718c83b15ca93b2f7d8002eeefd1f58bc1f5de
sidav <sidav94@gmail.com>
2017-12-10 19:53:51 +0300

    example.

--- Main.py
+++ Main.py
@@ -1,17 +1,19 @@
 from rk4 import SolverRK4
 from matplotlib import pyplot as plt
+from math import sin, cos
 
-f1 = lambda x, y, z: \
-    2 * x
+f1 = lambda t, x, y: \
+    sin(x)*(-.1*cos(x) - cos(y))
 
-f2 = lambda x, y, z: \
-    3 * x * x
+
+f2 = lambda t, x, y: \
+    sin(y) * (cos(x) - .1 * cos(y))
 
 solver = SolverRK4(f1, f2)
-xr, yr = solver.solve([0, 0], 0.001, 10)
+xr, yr = solver.solve([1, 1], 0.001, 30)
 plt.xlim(0, 10)
 plt.ylim(0, 10)
 plt.grid(linewidth=1)
-plt.plot(xr, yr[0])
-plt.plot(xr, yr[1])
+plt.plot(yr[0], yr[1])
+#plt.plot(xr, yr[1])
 plt.show()
